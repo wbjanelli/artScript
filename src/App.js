17352aecd044617be877eae8e67427d32658a031
William Janelli <williamjanelli@yahoo.com>
2023-12-18 12:04:24 -0800

    imageslider added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,50 @@
-import './App.css';
-import NavBar from './NavBar';
+// import './App.css';
+// import NavBar from './NavBar';
 
-function App() {
+// function App() {
+//   return (
+//     <>
+//       <main className="main">
+//         <NavBar websiteTitle={"artScript"} />
+        
+//       </main>
+//     </>
+//   );
+// }
+
+// export default App;
+
+import React from "react";
+import ImageSlider from "./ImageSlider";
+import NavBar from './NavBar';
+import './App.css';
+
+const App = () => {
+  const slides = [
+    { url: "http://localhost:3000/trippyshaman.jpg", title: "Chief" },
+    { url: "http://localhost:3000/nativetripeyes.jpg", title: "Shamon" },
+    { url: "http://localhost:3000/tiger8x.png", title: "Tiger" },
+    { url: "http://localhost:3000/trippynatove.jpg", title: "Trip" },
+    // { url: "http://localhost:3000/image-5.jpg", title: "italy" },
+  ]
+
+  const containerStyles = {
+    width: "500px",
+    height: "280px",
+    margin: "0 auto",
+  };
+
   return (
     <>
       <main className="main">
         <NavBar websiteTitle={"artScript"} />
-        
+        {/* <h1>Hello monsterlessons</h1> */}
+        <div style={containerStyles}>
+          <ImageSlider slides={slides} />
+        </div>
       </main>
     </>
   );
-}
+};
 
 export default App;
