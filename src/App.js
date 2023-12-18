1e465b80a7620ccc25bb3fbbdf51131c8939a807
William Janelli <williamjanelli@yahoo.com>
2023-12-18 13:43:12 -0800

    photoupdate

--- src/App.js
+++ src/App.js
@@ -5,12 +5,18 @@ import './App.css';
 
 const App = () => {
   const slides = [
+    { url: `${process.env.PUBLIC_URL}/genhis4k.png`, title: "genhis" },
+    { url: `${process.env.PUBLIC_URL}/snakecharmer.jpeg`, title: "snakemon" },
     { url: `${process.env.PUBLIC_URL}/nativetripeyes.jpeg`, title: "Chief" },
-    { url: `${process.env.PUBLIC_URL}/tiger8x.png`, title: "Tiger" },
-    { url: `${process.env.PUBLIC_URL}/trippynatove.jpeg`, title: "Trip" },
-    { url: `${process.env.PUBLIC_URL}/chillenstatue.jpeg`, title: "Chillin" },
     { url: `${process.env.PUBLIC_URL}/Oldchineseman.jpeg`, title: "OldMan" },
-    { url: `${process.env.PUBLIC_URL}/rhinobest.jpeg`, title: "rhino" },
+    { url: `${process.env.PUBLIC_URL}/whitetiger.jpeg`, title: "wt" },
+    { url: `${process.env.PUBLIC_URL}/chillenstatue.jpeg`, title: "Chillin" },
+    { url: `${process.env.PUBLIC_URL}/trippynatove.jpeg`, title: "Trip" },
+    { url: `${process.env.PUBLIC_URL}/tajm2.jpeg`, title: "taj" },
+    { url: `${process.env.PUBLIC_URL}/trippybuddha.jpeg`, title: "buddha" },   
+    { url: `${process.env.PUBLIC_URL}/TutAI.jpeg`, title: "tut" },
+    { url: `${process.env.PUBLIC_URL}/sadhu2.jpeg`, title: "sadhu" },
+    { url: `${process.env.PUBLIC_URL}/tiger8x.png`, title: "Tiger" },
   ]
 
   const containerStyles = {
